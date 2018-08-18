fa88784925a399bf89370e7b3d55a5c06c9d34a3
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2018-08-18 18:29:00 +0530

    finish getDistances

--- src/vigenere.ts
+++ src/vigenere.ts
@@ -1,35 +1,46 @@
 import { readFileSync } from "fs";
 
-import { getCount } from "./utils";
 import { Results } from "./types";
+import { getCount, getAllIndexes } from "./utils";
 
-const input =
-  process.argv.length > 2
-    ? process.argv[2]
-    : "./samples/vigenere-ciphertext.txt";
-const cipherText = readFileSync(input, "utf-8");
+const defaultFile = "./samples/vigenere-ciphertext.txt";
+const input = process.argv.length > 2 ? process.argv[2] : defaultFile;
+const ciphertext = readFileSync(input, "utf-8");
 
 const sortSequences = (limit: number = 4): string[] => {
   const regexString: string = `.{1,${limit}}`;
   const regex: RegExp = new RegExp(regexString, "gi");
 
-  return cipherText.match(regex);
+  return ciphertext.match(regex);
 };
 
-const getDistances = (items: string[], item: string): number[] => {
-  // TODO: calculate distances between instances of item
-  // console.log('items', items)
-  // console.log('item', item)
-  return [0];
+const getDistances = (
+  totals: Results,
+  sequence: string,
+  sequences: string[]
+): number[] => {
+  let distances: number[] = [];
+
+  const count = getCount(totals, sequence);
+  if (count > 1) {
+    const positions = getAllIndexes(sequences, sequence);
+    distances = positions.reduce((prevPosition, currPosition) => {
+      const distance = currPosition - prevPosition;
+      distances.push(distance);
+      return distances;
+    });
+  }
+
+  return distances;
 };
 
-const getStats = (items: string[]): Results => {
-  return items.reduce(
-    (totals, item) => ({
+const getStats = (sequences: string[]): Results => {
+  return sequences.reduce(
+    (totals, sequence) => ({
       ...totals,
-      [item]: {
-        count: getCount(totals, item),
-        distances: getDistances(items, item)
+      [sequence]: {
+        count: getCount(totals, sequence),
+        distances: getDistances(totals, sequence, sequences)
       }
     }),
     {}
@@ -53,9 +64,9 @@ const stats = filterSingleInstances(getStats(sequences));
 
 console.log(stats);
 
-// for (let i = 0; i < cipherText.length / limit - 1; i++) {
+// for (let i = 0; i < ciphertext.length / limit - 1; i++) {
 // const searchParam = new RegExp(`${sequences[i]}`, "gi");
-// const search = cipherText.match(searchParam);
+// const search = ciphertext.match(searchParam);
 
 // results.push(search);
 // }
