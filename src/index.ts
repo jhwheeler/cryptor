8f1bc6b551f410d8816e19cfcba6907763070742
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2018-08-18 15:23:14 +0530

    fix cli input

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { readFileSync } from "fs";
 import { formatText, getFrequency } from "./utils";
 
-const cipherText = readFileSync(process.argv[2], "utf-8");
+const input = process.argv.length > 2 ? process.argv[2] : "ciphertext.txt";
+const cipherText = readFileSync(input, "utf-8");
 const formattedText = formatText(cipherText);
 
 const frequencies = getFrequency(formattedText);
