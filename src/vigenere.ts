d06262e1b021f7ba04a07a6109b1b1685558dafc
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2018-08-18 15:23:51 +0530

    begin writing vigenere matching and sorting

--- src/vigenere.ts
+++ src/vigenere.ts
@@ -1,4 +1,21 @@
 import { readFileSync } from "fs";
+import { getFrequency } from "./utils";
 
-const cipherText = readFileSync("./vigenere.txt", "utf-8");
-console.log(cipherText);
+const input = process.argv.length > 2 ? process.argv[2] : "vigenere.txt";
+const cipherText = readFileSync(input, "utf-8");
+
+const results = [];
+
+for (let i = 0; i < cipherText.length; i++) {
+  const limit = 4;
+  const regexString = `.{1,${limit}}`;
+  const regex = new RegExp(regexString, "gi");
+
+  const matches = cipherText.match(regex);
+  const searchParam = new RegExp(`${matches[i]}`, "gi");
+  const search = cipherText.match(searchParam);
+
+  results.push(search);
+}
+
+console.log(results);
