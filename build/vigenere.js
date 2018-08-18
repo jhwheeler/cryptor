f15d67d6ef23d9819cc4ffa9507d227e28375578
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2018-08-18 14:21:52 +0530

    remove js

--- build/vigenere.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = require("fs");
-const cipherText = fs_1.readFileSync("./vigenere.txt", "utf-8");
-console.log(cipherText);
